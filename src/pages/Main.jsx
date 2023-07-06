ca5200ebc9f60b89e438b5972eab82518fc77d74
seungbeom1999 <khhds00@naver.com>
2023-07-06 19:37:48 +0900

    Feat: styled-component 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { styled } from "styled-components";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <StHeader>
+        <span>2023년 책 리뷰</span>
+        <button> 로그인 </button>
+      </StHeader>
+    </>
+  );
 }
 
 export default Main;
+
+const StHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 5px;
+  margin: 5px;
+`;
